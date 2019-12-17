6a45ecbf38a8230329380a52345050487af00e08
Niyazaki <zakkassabeh@hotmail.com>
2019-12-17 17:39:30 +0100

    still can be upgraded but works fine

--- optimisation.py
+++ optimisation.py
@@ -1,4 +1,5 @@
 import timeit
+import matplotlib.pyplot as plt
 
 numbers0 = [n for n in range(10)]
 
@@ -33,17 +34,31 @@ def computeSum(n):
     return result
 
 repeats = 1000
-
+yFor, yWhile, ySum =  [],[],[]
+x = [len(numbers0), len(numbers1), len(numbers2), len(numbers3)]
 for i in range(4) :
-    
+
     tFor = timeit.Timer('computeFor(numbers{})'.format(i), 'from __main__ import computeFor, numbers{}'.format(i))
     secFor = tFor.timeit ( repeats ) / repeats
     print ("{} : {} seconds". format ("for", secFor))
+    yFor.append(secFor)
 
     tWhile = timeit.Timer('computeWhile(numbers{})'.format(i), 'from __main__ import computeWhile, numbers{}'.format(i))
     secWhile = tWhile.timeit ( repeats ) / repeats
     print ("{} : {} seconds". format ("while", secWhile))
+    yWhile.append(secWhile)
 
     tSum = timeit.Timer('computeSum(numbers{})'.format(i), 'from __main__ import computeSum, numbers{}'.format(i))
     secSum = tSum.timeit ( repeats ) / repeats
     print ("{} : {} seconds". format ("sum", secSum))
+    ySum.append(secSum)
+
+plt.xlabel("Size of array")
+plt.ylabel("Time (s)")
+
+
+plt.plot(x,yFor, label ="f1 : For" )
+plt.plot(x,yWhile, label ="f2 : While ")
+plt.plot(x,ySum, label ="f3 : Sum" )
+plt.legend(loc='best')
+plt.show()
