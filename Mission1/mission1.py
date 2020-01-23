505eef85dc1bf6d1cb75cf6276f3a5ab45a31928
Niyazaki <zakkassabeh@hotmail.com>
2020-01-24 00:32:05 +0100

    added some ideas for mission1

---
+++ Mission1/mission1.py
@@ -0,0 +1,8 @@
+# Switch case
+# https://github.com/ThomasTheSpaceFox/Some-Small-python-pygame-apps/blob/43434ff976d49aba8f0d64e17bc79eb66788109d/lines1.py#L49
+
+# try !
+# https://github.com/gotonode/creatures/blob/10c6650027806e492088cc6b4cee68e72c63208f/main.py#L56
+
+# Bad smell or improvement needed ?
+# https://github.com/thannaske/diverse-ripe-atlas/blob/240d367567a7bbed60af359d065acd0bcddb8f05/get-probes.py#L47
